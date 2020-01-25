8fccce448364dc595e83c038170976346a7a56d9
Vitaliy <izhetalik@mail.ru>
2020-01-25 15:01:21 +0300

    add substracting
    i added substructing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from tools import adding
+from tools import adding, substracting
 
 
 a, b = map(int, input().split())
 
 print(adding(a, b))
+print(substracting(a, b))
--- tools.py
+++ tools.py
@@ -1,2 +1,6 @@
 def adding(a, b):
 	return a + b
+
+
+def substracting(a, b):
+	return a - b
